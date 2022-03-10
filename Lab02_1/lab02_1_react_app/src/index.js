17a216ad2c10d6456f8af691aa9c75605dbbf8f3
lyvietkhang <vietkhang92@gmail.com>
2022-03-11 01:21:10 +0700

    cách import bootstrap

--- Lab02_1/lab02_1_react_app/src/index.js
+++ Lab02_1/lab02_1_react_app/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//Ngoài module react và react-dom thì trong package.json còn nhiều modules khác cần download vào node_modules
+// https://stackoverflow.com/questions/44800993/how-to-download-and-install-all-node-modules-through-package-json
+// "npm install" sẽ đọc package.json và install node_modules trong trường hợp nộp bài không có node_modules
+
+//node: tất cả các tên modules (như react, bootstrap, reactstrap...) trong package.json đều đã npm install --save  vào node_modules
+import 'bootstrap/dist/css/bootstrap.min.css' //đường dẫn không cần node_modules, cứ Ctrl+click là tới file bootstrap.min.css
+//tiếp theo, trong App.js sẽ import reacstrap
+//Lưu ý: khi npm start thì module "react-scripts" sẽ được gọi, nếu Error: not recognized... thì phải xóa module đó trong node_modules rồi tải lại "npm install --save react-scripts" nó sẽ đọc package.json và tải theo version đó
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.render( //thường thì render( bên trong là return() , hàm return() có trong App)
   <React.StrictMode>
     <App />
   </React.StrictMode>,
