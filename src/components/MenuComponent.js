76436ff2abdc725b143a1d31bf491b1f5e58f0ab
lyvietkhang <vietkhang92@gmail.com>
2022-03-14 21:25:24 +0700

    add comments

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -22,7 +22,8 @@ class Menu extends React.Component {
 
     //render() là hàm bắt buộc có trong Component LifeCycle, và render() sẽ nhận tất cả các hàm mà return(Chuoi_JSX). Hàm thì bắt buộc chứa return()
     render() {
-        //Nhận props từ Main
+        //Nhận props.dishes từ Main và áp dụng map()
+        //nhận props.onClick từ Main và áo dụng cho callback của hàm map()
         console.log(this.props.dishes); //ok, lấy được mảng 4 phần tử từ class cha là Main gửi tới
         const menu=this.props.dishes.map(this.Trinh_bay_cac_mon_trong_menu);
         console.log(menu);
