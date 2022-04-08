f901ce9bc5c4dda597eb95a8e1c34bf801c79362
lyvietkhang <vietkhang92@gmail.com>
2022-04-08 07:36:51 +0700

    added Give us Comments

--- src/components/DishdetailsComponent.js
+++ src/components/DishdetailsComponent.js
@@ -30,6 +30,7 @@ const  DishDetails = (props) => { //nhận dữ liệu props.dish từ Main là 
     
                 <div className="col-12 col-md-5 m-1">
                     <RenderCommentItem comments={props.comments}/>
+                    <CommentForm/>
                 </div>   
             </div>
         </div>
