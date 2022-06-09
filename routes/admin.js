78d106dfc4f1a7450176e560f2014e55088f91ed
lyvietkhang <vietkhang92@gmail.com>
2022-06-09 18:33:56 +0700

    first commit to make master branch

---
+++ routes/admin.js
@@ -0,0 +1,30 @@
+const path=require('path');
+const express=require('express');
+const rootDir=require('../util/path');
+const router=express.Router();
+
+const products=[];
+
+
+//register routes for router
+//router.use()  cũng có chức năng như app.use()
+//
+
+console.log('rootDir:   '+rootDir)
+//  /admin/add-product =>GET because I only want to handle get requests 
+router.get('/add-product',(req,res,next) => {
+    res.render('add-product',{pageTitle:'Add Product',path:'/admin/add-product',formCSS:true,productCSS:true,activeAddProduct:true}); 
+    //doesn't have to match the path this route has, just a pattern I like.
+});
+
+//  /admin/add-product =>POST
+router.post('/add-product',(req,res,next)=>{   
+    console.log(req.body); 
+    products.push({title:req.body.title});
+    res.redirect('/'); 
+});
+
+//exports
+exports.routes =router; 
+exports.products=products; //đưa qua shop.js để output các products
+//we can export something, some object or array, a reference type
