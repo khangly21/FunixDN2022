60bab93acf2abcb4f65276a89e7e653e2f13c8e1
lyvietkhang <vietkhang92@gmail.com>
2022-06-13 19:28:12 +0700

    first commit to make master branch

---
+++ models/cart.js
@@ -0,0 +1,110 @@
+const fs = require('fs');
+const path = require('path');
+const { createBrotliCompress } = require('zlib');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    // Fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(
+        prod => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+      // Add new product/ increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static deleteProduct(id,productPrice){
+    //totalPrice cũng cần được updated luôn
+    //first of all I need to get my cart, so I need to read my file 
+    //copy code readFile đến đường dẫn cart.json rồi refactor 
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+           //nghĩa là tui không tìm thấy cart
+           //không có gì phải delete, so I just ignore 
+           return;
+        }
+        //no error here, continue
+        //I will update the cart
+        //Như 1 quy tắc, update thì tạo ra bản copy chứ không update trực tiếp lên dữ liệu 
+           ///tạo bản copy là đối tượng { }, sau đó lưu địa chỉ vào biến updatedCard  
+           /// take all the properties of the old cart and put them into a new object with that spread operator
+
+        //chuyển JSON string sang JS object
+        
+        const updatedCart={...JSON.parse(fileContent)}; 
+        //This will be fixed later, "cart" object does not exist here, we will need to (JSON.) parse that from fileContent to become JS object ! 
+           /// nếu Admin Products >> Delete thì sẽ báo lỗi "cart" is not defined here, vì local hàm callback không có biến cart được parse từ fileContent
+
+           
+        //I now want to update 2 thuộc tính của cart object là  both the (plan 1) mảng products and (plan 2) the totalPrice
+           ///(plan 2) total price should be reduced by the product price, however this would be incorrect because if we have the product in the cart three times =>  correct plan is it should be reduced by the product price times three.
+        //I just need the product to find out what the quantity is
+        console.log(id);
+        //Problem là id cần xóa có trong products.json nhưng chưa chắc có trong cart.json, sẽ dẫn tới biến product sau undefined và báo lỗi cannot read property "qty", nếu product có trong cả products.json và cart.json thì sẽ không báo lỗi
+        
+        const product=updatedCart.products.find(prod=>prod.id===id);
+        console.log(product); //nếu product muốn xóa có trong cart.json thì ok, ngược lại là undefined
+
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/undefined
+        if(product !== undefined){ //nghĩa là product được khai báo let product nhưng chưa được assign giá trị nào hết
+             //remember, we're storing the quantity in that qty field,
+             const productQty=product.qty;//nếu product undefined thì báo lỗi undefined qty
+             //(plan 1) cập nhật mảng products trong bản sao updatedCart 
+                 /// hàm filter lấy tất cả array items thỏa điều kiện prod.id!==id
+                 updatedCart.products=updatedCart.products.filter(prod=>prod.id!==id); 
+             //(plan 2) bấy giờ đã có totalPrice lẫn số lượng SP cần xóa
+             updatedCart.totalPrice=updatedCart.totalPrice-productPrice*productQty;
+             //This function will begin it`s writing always at the beginning of a file, so it will overwrite previous content.
+                /// Dẫn tới cart.json, updatedCart sẽ overwrite lên cart cũ , đó là lý do dùng object spread operator 
+                fs.writeFile(p,JSON.stringify(updatedCart),err=>{
+                    console.log(err);
+                })
+        }
+    })
+  }
+
+  static getCart(cb){
+     //tên hàm là getCart sẽ chính xác hơn là getProducts
+     //access my file and simply return the product IDs.
+     //cần hàm callback để được gọi xử lý ngay lập tức once I got products from readFile
+     //tuy nhiên kết quả readFile ở đây JSON.parse ra đối tượng cart { } chứ không phải mảng products 
+     //do đó cb sẽ xử lý đối tượng cart 
+     fs.readFile(p,(err,fileContent)=>{
+        const cart=JSON.parse(fileContent);
+        //obviously this can fail if we have no carts yet so I will check if we have an error
+        //xét xem err có tồn tại không
+        if(err){
+           cb(null);
+        }else{
+           cb(cart);
+        }
+        
+     })
+  }
+};
---
+++ models/product.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const path = require('path');
+
+//import cart model 
+const Cart=require('./cart');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      if (this.id) {//SP gọi save() đã có id , nghĩa là SP cần edit luôn có trong products.json (hover lên chữ Edit là biết id)
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {//SP gọi save() chưa có id, xảy ra khi http://localhost:3000/admin/add-product để thêm SP mới
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  static deleteById(id){
+      //I first of all need to find out which product to remove or update my array of products 
+      //copy code của hàm findById function
+      getProductsFromFile(products => {
+        //find Product có id tham số
+        const product=products.find(prod=>prod.id===id);
+
+        //tìm index của product trong mảng
+        //const productIndex = products.findIndex(prod => prod.id === id);
+        // I can update my products array such that this element is removed
+        
+        //Filter also takes an anonymous function and will return me ALL elements as part of a new array that do match the criteria
+
+        //câu hỏi là updatedProducts có phải mảng bản sao không
+           ///https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+           /// filter sẽ tạo new array thỏa ĐK và không làm thay đổi mảng ban đầu nên nó đáp ứng quy tắc không thay đổi dữ liệu trên đối tượng ban đầu khi update,If no elements pass the test, an empty array will be returned. 
+           /// console.log(mang_cu)
+           /// console.log(mang_moi)
+           //sau đó writeFile để ghi mang_moi đè lên mang_cu
+        const updatedProducts=products.filter(prod => prod.id !== id); //I  choose only the IDs is not equal to the ID I'm looking for
+        //so the item is not kept in the new array
+        //save my updatedProducts which are all products except for the one I want to delete back to the file products.json
+        //tham số thứ 3 sẽ throw any error object
+        console.log("updatedProducts:",updatedProducts);
+        fs.writeFile(p,JSON.stringify(updatedProducts),err=>{
+          //Sau khi tới products.json để ghi đè writeFile mảng mới lên mảng cũ, thì gọi hàm Cart.deleteProduct() để tới cart.json để ghi đè writeFile updatedCart lên Cart cũ
+          //if it does not throw any error, nghĩa là error == NULL 
+          if(!err){
+              //everything went fine then I also want to remove that product from the cart of course because I can't have it in a cart if the product doesn't exist anymore in products.json  
+              //next step in LAB4.18, we'll also work on the cart and make sure we can delete items from there
+              // let go to model>>cart.js 
+              // call cart delete product and pass the ID and I also need the price
+
+              Cart.deleteProduct(id,product.price);
+
+          }
+        });
+      });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
+};
