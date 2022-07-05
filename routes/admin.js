c95ac5ce4fd446379cd572efc1edde7acc2c1860
lyvietkhang <vietkhang92@gmail.com>
2022-07-05 17:45:24 +0700

    LAB7.7_phan1

---
+++ routes/admin.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
+
+const router = express.Router();
+
+//CÁCH SẮP XẾP: router.get nhóm chung với nhau vì là nhóm menus
+
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts); //giả sử chỗ edit sản phẩm, tải form lên với POST tới /admin/products thì Page Not Found
+
+
+// nhận form post từ Admin edits products /admin/products => POST
+router.post('/products', adminController.postEditProduct);
+
+// /admin/edit-product/:productId => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.get('/san_pham_vua_chinh_sua',function(req,res){
+    console.log("Hi");
+})
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+// /admin/edit-product => POST
+router.post('/edit-product', adminController.postEditProduct); //post req tới /edit-product thì sẽ được Controller nhận và xử lý bằng cách res.redirect('/admin/products');
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;
