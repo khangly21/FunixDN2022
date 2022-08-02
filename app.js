72ca5ff4c98d457aa092887745fb29f95fded641
lyvietkhang <vietkhang92@gmail.com>
2022-08-02 11:57:14 +0700

    first commit to make master branch

---
+++ app.js
@@ -0,0 +1,182 @@
+const path = require('path');
+
+const mongodb = require('mongodb');
+
+//lấy hàm express()
+const express = require('express');
+
+//mongoose instance
+const mongoose = require('mongoose');
+
+//lấy hàm session() để tạo session khi mà thấy yêu cầu login hợp lệ với thông tin csdl , thì tạo session như 'đánh dấu / tô màu' cho tất cả các req đơn lẻ của người đó trong suốt phiên lướt website
+const session=require('express-session'); 
+
+//let our express-session is stored inside document store on MongoDB Atlas (not limited memory)
+const MongoDBStore=require('connect-mongodb-session')(session); //actually gives you a function which should execute to which you pass your session. vế phải sẽ yield a constructor function to store in vế trái
+
+//https://expressjs.com/en/resources/middleware/body-parser.html
+const bodyParser = require('body-parser');
+//có các middlewares sau:
+   /// bodyParser.urlencoded([options]) => nhận đầu vào là <input name='x' value='y'/> và trả đầu ra là js object {name:'x',value:'y'} vào req.body. Sau đó tại server sẽ nhận req.body.<inputName></inputName>
+   /// bodyParser.json([options])
+
+const errorController = require('./controllers/error');
+
+//import User model / class (constructor) nên viết hoa U để tạo ra mongoose model với các magic functions 
+const User=require('./models/user');
+
+
+const MONGODB_URI='mongodb+srv://lyvietkhang_admin:FLC0EfhTqJHonvsI@khangserver0.w0azxjp.mongodb.net/testMongoose?';
+//nếu error thì có thể bỏ retryWrites=true
+
+//kích hoạt hàm express() để tạo express instance , từ đây có thể gọi hàm on this instance
+const app = express(); //express là hàm được exported bởi express module
+//với app thì có thể set up / register các middleware (stack) (cũng gọi là miniapp vì nhận req và trả res) , VD: session()
+
+// now we can setup and use a store as a session storerage
+const session_store= new MongoDBStore({
+    //inside this constructor you can pass some options in js object
+    //which options could that database store require?
+    //it will require a connection string (lấy connection string của mongoose, vì dùng 2 lần nên cho vô 1 biến toàn cục) because it needs to know in which database, on which database server to store your data.
+    uri:MONGODB_URI,
+    // you need to to define the collection where your sessions will be stored and the name is up to you
+    collection:'sessions'
+    //more info. when expires, can be cleaned up automatically by mongodb?
+}) ; //MongoDB là 'document-oriented database' or 'document store'
+
+app.set('view engine', 'ejs');
+
+//app.set('views', 'views');
+app.set('views', path.join(__dirname, 'views'));
+
+//để sử dụng các router, phải register chúng cho app.js
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const autRoutes=require('./routes/auth');
+
+//urlencoded() return middleware that only parses urlencoded body, thường dùng trong form post
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//provide this public directory as static resource with this simplest API: app.use(static('public' ))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+      
+      secret:'my secret',
+      resave:false,
+      //set resave to false, this means that the session will not be saved on every request that is done,  on every response that is sent. But but only if something changed in the session => improve performance and so on
+      saveUninitialized:false,
+      //req.session:"session xin chào",  //sẽ bị báo lỗi
+      store:session_store  //ok. truy cập Mongodb lấy session ? Không, là tạo store chứa session. Có thể dùng redis store: https://stackoverflow.com/questions/21212828/cannot-access-req-session-variables-in-express-nodejs
+}));
+
+//Assume a request coming (with or without a session cookie), session middleware is initialized and session data is loaded using the session cookie and we're now ready to use the req.session
+
+//So BY the time we reach this general middleware, we'll have our session data loaded. 
+   ///means that now we just want to use that session data to load our real user, to create our MONGOOSE user model (có các hàm nữa) and how do we do that?
+// thầy gọi là "general" middleware
+//How to do that?Well we don't need to reach out to the database again
+
+
+app.use((req,res,next)=>{//this middleware is triggered for every request 
+    if (!req.session.user) { 
+      //checking for the existence of req.session.user trong mỗi req tới cánh cổng này
+      //Lúc này chưa thực hiện postLogin, hoặc sau khi log out thì req.session chưa có biến user
+      //Chú ý là req.session lúc nào cũng giữ biến cookie, còn lại thêm biến gì tùy người dùng
+      console.log("req của getLogin lúc đang không KYC",req.session)
+      // if I don't have a user stored in my session, then I can just call next and I return next so that the code thereafter, this one will not be executed
+      return next();
+    }
+    //dùng Model của Mongoose để đón dữ liệu của req.session.user 
+
+    //static: session.user là dữ liệu không bị hủy sau bất cứ res.redirect nào trước đó
+    //dynamically thứ nhất , bất cứ req nào tới middleware session() đều làm đầu vào của nó và được gán thành session.user , do đó bất chấp có redirect 
+    //duynamically thứ hai, findById() nhận id tùy vào postLogin
+    User.findById(req.session.user._id)  //Điều kiện là MongoDB phải có req.session.user và dùng model User để tra cứu trong bảng "users", nếu không sẽ báo lỗi 500 cannot read _id of undefined ngay cả với nhấn Login req bị chặn vì lỗi req tới đây
+    .then(user => { // I get back a mongoose model user which I store in request user with Mongoose methods
+        
+        req.user=user;  //sẽ dùng Mongoose model để có các Mongoose model chứ không dùng req.session.user nữa
+        
+        next(); 
+    })
+    .catch(err => console.log(err));
+})
+
+//API= how to communicate with the system you are creating  (Bản mô tả cách sử dụng phần mềm). 
+    /// VD để mô tả cách sử dụng một máy chủ mới được tạo, thì có thể sử dụng các quy tắc của Express (do đó có câu Express is the web server used by the api)
+    /// VD group facebook Pi360do lập các mô tả quy tắc cho người vào nhóm phải tuân theo (VD không cài link vào, không direct message ...)
+// api route = A route within an API is a specific path to take to get specific information or data out of
+   
+//register routes with "leading filters" for app  
+//công thức là app.use(path,middleware callback(s))
+//By piecing together many of these API routes (also known as endpoints) we can build a functional API that can drive our applications
+  /// (nhóm các API có cùng chức năng lại sẽ tạo 1 functional API (function-oriented API)  giải thích rõ ràng cách dùng ứng dụng)
+//different types of API routes (known as endpoints)
+app.use('/admin', adminRoutes);
+//các middlewares sau no leading filter so every request will go in there and anything which is not found in the shop routes will therefore go into the auth routes
+app.use(shopRoutes);
+app.use(autRoutes);
+
+app.get('/errors',(req,res)=>{  //Note: Khi yêu cầu http://localhost:5002/errors thì có endpoint '/errors' tồn tại trên server do đó không báo lỗi 404, nhưng req.locals là nguồn tài nguyên không có trong server nên báo lỗi 500 với nhiều đường dẫn ảnh hưởng bảo mật, do đó cần có middleware để render ra view 500.ejs
+  //console.log("doi tuong req: ",req)
+
+  //https://www.geeksforgeeks.org/express-js-res-send-function/
+  //res.send trả về 1 object, không nhận callback 
+  res.send(req.locals.message); //req.locals không tồn tại . Nói cách khác không tìm ra resource này trên middleware của server 
+})
+
+//Error handler có 4 tham số (err do middleware trước nhả ra, req,res,next. Nếu các middleware trước kết thúc req-res cycle thành công thì không có err nào nhả ra, nên không được ghi tham số err). Còn các middleware bình thường thì param đầu tiên phải là request object
+
+//handle 404 Page not found (dĩ nhiên không có error nào nên không có biến đầu tiên là error)
+//https://stackoverflow.com/questions/43356705/node-js-express-error-handling-middleware-with-router
+app.use(errorController.get404); //nếu trong adminRoutes và shopRoutes xóa tất cả routes thì khi req vào port 3000 thì được server bắt được, so sánh không có một matching route nào hết nên đi vào API get404 nơi đó có res status 404 và hiển thị view 404
+
+
+//handle 500 Internal Server Error
+app.use(errorController.get500);
+
+// mongoose.connect opens mongoose 's default connection to MongoDB
+//Mongoose creates a default connection when you call mongoose.connect(). You can access the default connection using mongoose.connection
+
+
+//console.log("hey Randyyyy", typeof(new mongodb.ObjectId(24)) , new mongodb.ObjectId(24));  //object 00000018583af60838beac1f
+const object={
+  //local variable, nên phải được tiếp cận bằng object.name
+   name:'Kahn',
+   age:8
+};
+//console.log(object, typeof(object.age)); // {name:'Kahn',age:8 }  number. Như vậy không phải được console.log là thành string
+
+//mongoose là "MongoDB adapter"
+//nếu không dùng các options trong js object thì dùng port >5000 cho high versions of mongoose and mngodb để giải quyết vấn đề db connection times out
+mongoose.connect(MONGODB_URI)
+  /*
+      , { 
+       maxIdleTimeMS: 80000,
+       serverSelectionTimeoutMS: 80000,
+       socketTimeoutMS: 0,
+       connectTimeoutMS: 0
+      }
+
+      thì vài lúc kết nối được, còn lúc không được thì báo lỗi: mongoNetworkError: connect ETIMEDOUT 52.77.81.2:27017
+      Do đó nên đổi port từ 3000 sang trên 5000
+  */
+        
+        .then(result=>{ 
+ 
+            //app tạo server nghe ngóng (Nếu dòng này không hiện ra là biết Mongoose cần update IP address)
+            //app sẽ không chạy nếu có MongooseServerSelectionError (do IP thay đổi)
+            app.listen(5002,function(){
+
+              console.log("server is using app and listening  on port");
+              console.log("Làm sao Express app sử dụng mongoose instance để map đối tượng javascript vào document?");
+              console.log("Mỗi req chỉ tới 1 port, Nếu req tới port 3000 mà không có matching route thì res view 404, còn req mà tới port 5000 thì không có server nào và báo 'This page cannot be reached' ")
+            
+            });
+        })
+        .catch(err=>{
+          console.log(err); //potential error I might be getting when trying to get connected
+        })
+//we already have everything in place we need to connect
+//mongoose as I mentioned will manage that one connection behind the scenes for us
+//so that in other places where we start using mongoose, we use that same connection we set up here, really convenient of course
---
+++ controllers/auth.js
@@ -0,0 +1,202 @@
+const bcrypt= require('bcryptjs'); 
+
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+      //có 2 trường hợp: trước khi postLogin và sau khi postLogin , cả 2 TH đều nhận Session views là req.session.views
+         /// nhưng sau khi logout xong thì còn req.session.views hay không??
+      // có 2 sự kiện trỏ tới route '/login' bằng cách res.redirect('/login)
+         /// postSignup
+         /// postLogout
+      console.log("session của getLogin ( 2 trường hợp: trước khi postLogin và sau khi postLogin ): \n",req.session); //trả về đối tượng Session đã tạo lúc npm start
+      console.log(req.session.isLoggedIn);
+
+      //trường hợp gõ http://localhost:5002/login mà không thông qua http://localhost:5002/ thì chưa có session cookie nào
+      let sessionViews=req.session.views? req.session.views:0;  //let sessionViews=req.user? req.session.views:0; là sai vì trước khi postLogin chưa có user chỉ có req.session.views, chỉ có sau khi postLogin thì mới có thêm user và kycedViews
+      //console.log("sau khi postLogout thì không còn session tương ứng nữa  \n",sessionViews)
+      res.render('auth/login', {
+            path: '/login', //để hightlight nav Login khi path là /login
+            pageTitle: 'Login', //nếu không nhận được "Logint" sẽ ra "Page Not Found"
+            sessionViews:sessionViews,
+            isAuthenticated:req.user  //nối tiếp chuỗi khởi đầu từ postLogin,,ok, trang login cũng cần navigation.ejs
+      });
+};
+
+
+
+exports.postLogin = (req, res, next) => {
+      //Vì sao trước khi postLogin không có session cookie nào, và MongoDB không có session nào, cho tới khi postLogin??
+      console.log("req do client gửi tới , có biến session không ??: \n",req); 
+      //https://stackoverflow.com/questions/55558402/what-is-the-meaning-of-bodyparser-urlencoded-extended-true-and-bodypar
+      //đầu tiên khi Login, body có {email:'',password:''}
+      //It helps you to create object from the form inputs
+      //<input type="email" name="email" id="email">
+      //<input type="password" name="password" id="password">
+
+      //sau khi đăng ký Signup thì trong CSDL collection users có id người dùng --> cần findById một cách dynamic
+      User.findById('62e7d67be2660d2622f0f371')  
+          .then(user => { //this is mongoose user inheriting all mongoose methods
+
+
+            //Đã có req.session do incoming req tới middleware session() , bây giờ là thời điểm gán các biến đánh dấu đã xác thực vào session:
+
+            //trước kia như thẦy NgoTuanAnh nói, middleware session() đã tạo collection sessions nhưng chưa có document nào
+            //thì ứng với 1 req sẽ tạo ra 1 session document có dữ liệu {isLoggedIn và user và cookie}   đồng thời cho browser 1 session cookie
+            //Important note: hàm session() trong app.js chỉ là để tạo session middleware và sau đó npm start sẽ tạo collection tên "sessions" trên MongoDB và chưa có doc nào,  chỉ có 1 req vào 1 port 5002 và chỉ có req của postLogin thì mới tạo được session document trong "sessions" và gửi res chứa session cookie cho client
+             req.session.isLoggedIn=true;
+
+             //CHÌA KHÓA CHỖ NÀY, thử req.user = user thì lúc nào cũng tới màn hình '/' với KYC:false vì lúc đó mất dấu không biết user nào
+             req.session.user = user;  
+             //Problem là việc thêm property lên MongoDB sẽ chậm hơn việc redirect vì 2 công việc đó là độc lập nhau, dẫn tới trang / chưa có 2 menu của admin vì req.session chưa có user nên có vẻ chưa login 
+             //Solution: dùng callback để đảm bảo save trên mongodb trước khi redirect
+             req.session.save((err)=>{
+                  //if an error exists, but most of the time that should not be the case 
+                  console.log(err);
+             })
+             //Nếu dùng req.user=user thì res.redirect sẽ hủy req trước đó nên user không chuyển xuyên suốt các routes.with session, we can share across request, this is a way of using session to store user mongoose object
+             //Tuy nhiên, req.user có thể gọi hàm của class User, còn req.user trên MongoDB chỉ chứa data không chứa methods nào nên không gọi hàm được
+             //Do đó kinh nghiệm là trước res.redirect thì đừng dùng biến req để lưu , mà dùng req.session
+             res.redirect('/');
+             //Tới '/' nhưng req.user không mất đi, vì req gặp session() thì sẽ có req.user ,sẽ luôn nằm trong bất cứ req nào tới app, nhưng req.user sẽ không truy cập các hàm của mongoose được
+             //Tuy nhiên cần theo dõi tiếp sau session middleware còn có 1 middleware đón req nữa
+          })
+          .catch(err => console.log(err));
+      //console.log("req sau khi gắn session: \n",req)
+};
+
+exports.postLogout = (req, res, next) => {  //cho err là tham số đầu tiên được k
+      req.session.destroy(err => { //err chỉ là tham số, không phải if nên res.redirect('/login'); có thể vào callback function
+        console.log(err);
+        res.redirect('/login');
+      });
+};
+
+
+exports.postSignup=(req,res,next)=>{
+    //we want to extract the information from the incoming request, với sự trợ giúp của body-parser middleware which turn input into js object, gắn vào req.body
+    const email=req.body.email; //see if we find a user where the email field holds a value equal to the email we're extracting here.
+    const password=req.body.password;
+    const confirmPassword=req.body.confirmPassword;
+    //ok we got typical form and now typically we will validate the user input (ở Frontend đã check dữ liệu valid hay invalid, còn backend sẽ xác thực với csdl)
+    //Tui sẽ dành 1 module về form input validation --> would check is this a valid email address and do password and confirm password match
+    //For now we'll ignore this and that means for now you of course don't have to enter a valid email and you don't have to enter a valid password, you don't have to confirm it
+    //but of course you should do all that to have a realistic testing environment, we'll add validation later.
+    User.findOne({email:email})
+        .then(userDocumentModel=>{
+            //then() trả về 1 Mongoose object hoặt không object nào (lúc này userDocumentModel undefined vì không chứa địa chỉ của đối tượng nào hết).
+            //cũng như req.session.destroy( (err) => {console.log(err)} ) được mentor NgoTuanAnh giải thích là: destroy nó sẽ gọi callback và nếu có lỗi thì nó sẽ gọi callback mà b đã viết và pass cái lỗi đó vào param thứ nhất. Nơi mà b đặt là err nhé. Trong cb thì b đã log err đó ra nè. Nếu chỗ đó k có lỗi thì err sẽ là undefined
+            //tương tự, catch(err=>console.log(err)) 
+            //https://attacomsian.com/blog/promises-javascript
+                /*
+                    A promise in JavaScript is an object that MAY produce a single value upon (tùy vào) completion (or failure) of an asynchronous operation. It works as a proxy (???) for a value not necessarily known (giả định xảy ra ở tương lai, undefined vào lúc viết, cũng có thể undefined sau khi Promise is not resolved/fulfilled/the promise completed successfully) at the time when the promise was created. Promises allow you to attach (bằng cách dùng hàm then() hay catch() của Promise object ) callback handlers to handle the FUTURE asynchronous success value or failure reason.
+                    Promises are not meant to replace the callbacks. Instead, they SIMPLIFY the chaining of functions, making it easier to read and maintain the code. Không thể lồng nhiều callback với nhau được sẽ khó đọc và sửa lỗi
+                    What is a Callback?  JavaScript is a single-threaded ASYNchronous programming language, callback functions are used to make it work as (if it is) an SYNCchronous programming language
+                    Why used a Callback? Unlike most of the functions which immediately return some value, functions with callbacks take some time to produce a result. Generally, callbacks are used for time-consuming tasks such as downloading files, reading files, sending emails, fetching data from a database, etc.
+                */
+            //if user doc exists, so if this is not undefined
+            if(userDocumentModel){
+                //do have a user then I certainly don't want to create a new one with the same e-mail.
+                return res.redirect('/signup'); //Anh cần đăng ký mới. Nhưng problem là chạy dần xuống dưới sẽ gặp res.redirect('/login') và bị báo lỗi "Cannot set headers after they are sent to the client"
+
+                //we just redirect without showing an error message,
+                //chú ý: 1 function chứa if thì muốn thoát nhánh if thì phải return không hay gì đó cho function: https://stackoverflow.com/questions/20139292/exit-from-if-block-in-javascript
+                //sau khi thoát ra (break out) khỏi if thì có userDocumentModel => res.redirect('/signup')  là viết tắt của (userDocumentModel)=>return res.redirect('/signup')
+            }
+            //đã successfully passed this check, we know that no user with that e-mail exists yet and therefore we can create a new one
+
+            //I store a hashed password, first param is string to be hashed, second param is the salt value which is how many rounds for hashing,  a value of 12 is accepted as highly secure.
+            //this is an asynchronous task (task cần nhiều thời gian không thực hiện bởi NodeJS mà bởi engineV8 của Chrome HOẶC Edge Chrome) and therefore this actually gives us back a promise.
+            
+            return bcrypt.hash(password,12);  //trả về "future" value là futureHashedPassword
+            
+        })
+        .then(futureHashedPassword=>{
+            console.log("Beginning: futureHashedPassword khi signup với email đã có trong CSDL Như test2@test.com: \n",futureHashedPassword) //undefined, vì res.redirect rồi nhưng vẫn thực hiện then() và vấn đề là bcrypt.hash(password,12)  chưa bao giờ được thực hiện
+            const user=new User({
+                //left side is the field, right side is the data extracted from req.body 
+                email:email,
+                password:futureHashedPassword, //with that, we dramatically improve the performance
+                //tới đây thấy class User không có password, nên có thể sửa schema lại để tạo class khác có các thuộc tính email,password, mặc dù lúc đầu khi users collection rỗng thì đã mặc định tạo user đầu tiên có {name,email,cart}
+                cart:{items:[]}
+            });
+            //Nếu để trống form Signup mà submit luôn thì vi phạm Schema User , lúc đó không có user model nào được tạo
+            console.log(user); //cố tình log 1 object as string, có bị báo lỗi là [object Object] ??
+            /*
+                  //Mặc dù futureHashedPassword bị undefined, nhưng vẫn ra kết quả là:
+                  {
+                      email: 'test2@test.com',
+                      cart:{items:[]},
+                      _id:new ObjectId("62e8538e...")
+                  }
+                  ServerResponse.header: Cannot set headers after they are sent to the client   
+                  ok,user saved
+            */
+            return user.save(()=>{console.log("đã lưu")}); //IMPOTANT NOTE: callback của save() có thể là function(){console.log("ok, user saved. I am the callback happening before the next then")} NHƯNG SẼ BÁO LỖI "Error: Cannot set headers after they are sent to the client"
+            //return a Promise object. But WHY? để đảm bảo async save() mất thời gian thực hiện xong đã rồi nếu không có lỗi nào thì sẽ chạy callback trong then tiếp theo
+            // return this so that we can chain another then block here which will be called once this save action completed (xem bài Two tiny improvements)
+        })
+        .then(result=>{
+            console.log("result sau khi save: \n",result)//undefined
+        })
+        .then(
+            result=>{
+                console.log("result from nothing: \n",result)
+                return User.findOne({email:'test2@test.com'})
+            }
+        )
+        .then(
+            //Không có lỗi nào thì vào callback trong then()
+            //then trước đó trả về đối tượng res.redirect('/') || user.save() 
+            //thời điểm này tui biết user did sign up
+            
+            result=>{
+                 // this function here will be executed once saving is done.
+                 console.log("result của User.findOne : \n",result) //undefined
+                 console.log("OK. đã save xong")
+                 res.redirect('/login')
+                 //because after signing up, you need to login.
+                 //I also have no server error (500)
+            }
+            
+        )
+        .catch(err=>console.log(err))  //we also might get an error which I'll simply log here (nếu không có err object nào được throw ra thì err undefined do biến này không chứa địa chỉ của đối tượng nào)
+
+};
+
+
+// exports.postSignup = (req, res, next) => {
+//     console.log(req.body);
+//     const email = req.body.email;
+//     const password = req.body.password;
+//     const confirmPassword = req.body.confirmPassword;
+//     User.findOne({ email: email })
+//       .then(userDoc => {
+//         if (userDoc) {
+//           return res.redirect('/signup');
+//         }
+//         return bcrypt.hash(password, 12);
+//       })
+//       .then(hashedPassword => {
+//         const user = new User({
+//           email: email,
+//           password: hashedPassword,
+//           cart: { items: [] }
+//         });
+//         console.log(user);
+//         return user.save(function(){console.log("ok, user saved")});
+//       })
+//       .then(result => {
+//         res.redirect('/login');
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   };
+  
+exports.getSignup=(req,res,next)=>{
+      res.render('auth/signup',{
+           path:'/signup',
+           pageTitle:'Signup',
+           isAuthenticated:false
+      });
+}
